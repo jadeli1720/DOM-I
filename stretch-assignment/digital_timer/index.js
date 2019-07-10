012d19f30e01b009e1e7b19527eb1b95efb79784
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-07-09 23:17:15 -0600

    Modified /digital_timer/index.js: added a lock (as suggested by Henry Blevins) so that the start button is disabled when clock is running. Now need to set numbers to red when timer gets to 10s mark and then reset to black

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -34,30 +34,40 @@
         2) set msTens textContent to the above
         3) Use Math.floor() on msHundreds so that the value after the decimal from the hundreds of the hundreds place is the value of the 10s place rounds it down.
         4) repeat process for  secondOnes and secondTens
-7) Need timer to restart/reset when the start button is clicked by simply seting the timer variable to 0 in our click event listener
+
+7) Need timer to restart/reset when the start button is clicked by simply setting the timer variable to 0 in our click event listener
+
+8) Disable button while timer is running
 
 */
 
-const button = document.querySelector('button'),
-    secondTens = document.querySelector('#secondTens'),
-    secondOnes = document.querySelector('#secondOnes'),
-    msHundreds = document.querySelector('#msHundreds'),
-    msTens = document.querySelector('#msTens')
+const button = document.querySelector("button"),
+    secondTens = document.querySelector("#secondTens"),
+    secondOnes = document.querySelector("#secondOnes"),
+    msHundreds = document.querySelector("#msHundreds"),
+    msTens = document.querySelector("#msTens");
 
 let timer = 0;
+let lock = false;
 
 button.addEventListener("click", event => {
-    timer = 0;
-    event.preventDefault();
-    const interval = setInterval(() => {
-        timer += 10;
-        // console.log(timer); //so we can see the timer incrementing this will keep running though in console.
-        msTens.textContent = (timer / 10) % 10;
-        msHundreds.textContent = Math.floor(timer / 100) % 10;
-        secondOnes.textContent = Math.floor(timer / 1000) % 10;
-        secondTens.textContent = Math.floor(timer / 10000) % 10;
-        if (timer >= 3000) {
-            clearInterval(interval);
-        }
-    }, 10);
+    if (!lock) {
+        lock = true;
+        timer = 0;
+        //reset color when clicking on timer
+        event.preventDefault();
+        const interval = setInterval(() => {
+            timer += 10;
+            // console.log(timer); //so we can see the timer incrementing this will keep running though in console.
+            msTens.textContent = (timer / 10) % 10;
+            msHundreds.textContent = Math.floor(timer / 100) % 10;
+            secondOnes.textContent = Math.floor(timer / 1000) % 10;
+            secondTens.textContent = Math.floor(timer / 10000) % 10;
+            if (timer >= 10000) {
+                lock = false;
+                clearInterval(interval);
+                //set numbers to red using css
+            }
+        }, 10);
+    }
 });
